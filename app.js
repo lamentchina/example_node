e2c22f820124609e71c0cab06e88ff627e327b32
tom.zhang <mrzhang@msn.com>
2014-10-22 05:33:31 +0800

    udpate

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 "use strict"
-var koala = require('koala');
-var app = koala();
+var koa = require('koa')
+  , router = require('koa-router')
+  , app = koa();
+
+app.use(router(app));
+
+app.get('/idx/:id', function *(next){
+  console.log('idx->id')
+  this.body = 'idx:id';
+})
+
 app.use(function *(){
   this.body =  'hellow koa';
 });
+// app.port = 9501;
 app.listen(9501);
