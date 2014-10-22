64a7e641254578c77843ac59224288dd7c62c778
tom.zhang <mrzhang@msn.com>
2014-10-22 17:49:42 +0800

    update router

--- app.js
+++ app.js
@@ -1,17 +1,27 @@
 "use strict"
 var koa = require('koa')
-  , router = require('koa-router')
+  , render = require('./lib/render')
+  , router = require('koa-route')
+  , logger = require('koa-logger')
   , app = koa();
+this.port = 9501;
+app.use(logger());
 
-app.use(router(app));
+// app.use(router(app));
+// app.get('/idx/:id', function *(next){
+//   var _id = this.params.id;
+// });
+app.use(router.get('/idx/:id', idx));
 
-app.get('/idx/:id', function *(next){
-  console.log('idx->id')
-  this.body = 'idx:id';
-})
+function *idx() {
+  this.body = yield render('body');
+};
 
 app.use(function *(){
-  this.body =  'hellow koa';
+  // this.body =  'hellow koa';
+  this.body = yield render('footer')
 });
 // app.port = 9501;
-app.listen(9501);
+app.listen(this.port);
+
+console.log('listening on port: '+ this.port);
