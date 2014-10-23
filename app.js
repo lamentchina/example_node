970adfb8c0a082b3cc210129c0cfc350cb4f1b99
tom.zhang <mrzhang@msn.com>
2014-10-23 21:08:08 +0800

    update

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 "use strict"
 var koa = require('koa')
+  , path = require('path')
   , render = require('./lib/render')
   , ejs = require('koa-ejs')
   , router = require('koa-route')
@@ -8,8 +9,22 @@ var koa = require('koa')
 this.port = 9501;
 app.use(logger());
 
-module.exports = function(app){
-};
+// module.exports = function(app){
+//   // ejs 配置
+//   ejs(app, {
+//     root: path.join(__dirname, 'view/frame'),
+//     layout: 'body',
+//     viewExt: 'html',
+//     cache: false,
+//     open: '<ejs>',
+//     close: '</ejs>'
+//     /*debug: true,
+//     filters: filters*/
+//   });
+// };
+
+ejs.open = '<ejs>';
+ejs.close = '</ejs>';
 
 // app.use(router(app));
 // app.get('/idx/:id', function *(next){
@@ -21,12 +36,32 @@ function* idx() {
   this.body =
     yield render('body');
 };
+console.log(ejs)
+
+// app.use(function *() {
+  // var users = [{name: 'Dead Horse'}, {name: 'Jack'}, {name: 'Tom'}];
+  // yield this.render('content', {
+  //   users: users
+  // });
+// });
 
 app.use(function*() {
-  // this.body =  'hellow koa';
-  this.body =
-    yield render('footer')
+//   console.log(this.ip);
+//   // this.body =  'hellow koa';
+//   this.body = yield render('body');
+    yield this.render('body', {
+
+    })
+//
 });
+
+// app.use(function *() {
+//   var users = [{name: 'Dead Horse'}, {name: 'Jack'}, {name: 'Tom'}];
+//   yield this.render('content', {
+//     users: users
+//   });
+// });
+
 // app.port = 9501;
 app.listen(this.port);
 
